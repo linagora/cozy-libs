dba7d2eaa2f5f3b130749f4f273afd723d08abcb
doubleface <christophe@cozycloud.cc>
2026-01-14 09:09:44 +0100

    feat: Remove panel title
    Remove "usefull information" title

--- packages/cozy-viewer/src/Panel/PanelContent.jsx
+++ packages/cozy-viewer/src/Panel/PanelContent.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 
 import Paper from 'cozy-ui/transpiled/react/Paper'
 import Stack from 'cozy-ui/transpiled/react/Stack'
-import Typography from 'cozy-ui/transpiled/react/Typography'
 
 import getPanelBlocks, { getPanelBlocksSpecs } from './getPanelBlocks'
 import { withViewerLocales } from '../hoc/withViewerLocales'
 import { useViewer } from '../providers/ViewerProvider'
 
-const PanelContent = ({ t }) => {
+const PanelContent = () => {
   const { file, isPublic, componentsProps, isReadOnly } = useViewer()
 
   const panelBlocks = getPanelBlocks({
@@ -19,13 +18,6 @@ const PanelContent = ({ t }) => {
 
   return (
     <Stack spacing="s" className={cx('u-flex u-flex-column u-h-100')}>
-      <Paper
-        className="u-flex u-flex-items-center u-h-3 u-ph-2 u-flex-shrink-0"
-        elevation={2}
-        square
-      >
-        <Typography variant="h4">{t('Viewer.panel.title')}</Typography>
-      </Paper>
       {panelBlocks.map((PanelBlock, index) => (
         <Paper
           key={index}
