b315fcd05a2a4bc935ecf08092f3130bfc65968d
doubleface <christophe@cozycloud.cc>
2026-01-14 09:07:48 +0100

    feat: Add antivirus panel block
    This panel block display the antivirus status of a file.

---
+++ packages/cozy-viewer/src/Panel/Antivirus.jsx
@@ -0,0 +1,110 @@
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
+import InfoIcon from 'cozy-ui/transpiled/react/Icons/InfoOutlined'
+import ShieldIcon from 'cozy-ui/transpiled/react/Icons/Shield'
+import ShieldCleanIcon from 'cozy-ui/transpiled/react/Icons/ShieldClean'
+import ShieldInfectedIcon from 'cozy-ui/transpiled/react/Icons/ShieldInfected'
+import SpinnerIcon from 'cozy-ui/transpiled/react/Icons/Spinner'
+import Link from 'cozy-ui/transpiled/react/Link'
+import List from 'cozy-ui/transpiled/react/List'
+import ListItem from 'cozy-ui/transpiled/react/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import Popover from 'cozy-ui/transpiled/react/Popover'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+
+import { withViewerLocales } from '../hoc/withViewerLocales'
+
+const Antivirus = ({ file, t }) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handleInfoClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+
+  const fileState = file?.antivirus_scan?.status
+  let icon = null
+  let text = null
+  let isError = false
+  if (['pending', 'error', 'skipped'].includes(fileState)) {
+    icon = SpinnerIcon
+    text = t('Viewer.panel.antivirus.scanning')
+  } else if (fileState === 'clean') {
+    icon = ShieldCleanIcon
+    text = t('Viewer.panel.antivirus.scanned')
+  } else if (fileState === 'infected') {
+    icon = ShieldInfectedIcon
+    text = t('Viewer.panel.antivirus.infected')
+    isError = true
+  }
+
+  if (!icon || !text) {
+    return null
+  }
+  return (
+    <List>
+      <ListItem>
+        <ListItemIcon>
+          <Icon icon={icon} color={isError ? 'var(--errorColor)' : ''} />
+        </ListItemIcon>
+        <ListItemText>
+          <Typography color={isError ? 'error' : 'default'}>{text}</Typography>
+        </ListItemText>
+        <ListItemIcon>
+          <IconButton onClick={handleInfoClick} size="small">
+            <Icon icon={InfoIcon} />
+          </IconButton>
+        </ListItemIcon>
+      </ListItem>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+      >
+        <div className="u-p-1 u-flex u-maw-5">
+          <div className="u-mr-half u-flex-shrink-0">
+            <Icon icon={ShieldIcon} color="var(--primaryColor)" />
+          </div>
+          <div>
+            <Typography variant="subtitle1" className="u-mb-half">
+              <strong>{t('Viewer.panel.antivirus.info.title')}</strong>
+            </Typography>
+            <Typography variant="body2" className="u-mb-1">
+              {t('Viewer.panel.antivirus.info.description')}
+            </Typography>
+            <div className="u-ta-right">
+              <Link
+                href="https://twake.app"
+                target="_blank"
+                rel="noopener noreferrer"
+                color="primary"
+              >
+                {t('Viewer.panel.antivirus.info.learnMore')}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Popover>
+    </List>
+  )
+}
+
+Antivirus.propTypes = {
+  file: PropTypes.object.isRequired,
+  t: PropTypes.func
+}
+
+export default withViewerLocales(Antivirus)
--- packages/cozy-viewer/src/Panel/getPanelBlocks.spec.jsx
+++ packages/cozy-viewer/src/Panel/getPanelBlocks.spec.jsx
@@ -66,6 +66,10 @@ describe('getPanelBlocks', () => {
 describe('getPanelBlocksSpecs', () => {
   it('should return the specs of the blocks to display in the panel', () => {
     expect(getPanelBlocksSpecs()).toEqual({
+      antivirus: {
+        condition: expect.any(Function),
+        component: expect.anything()
+      },
       qualifications: {
         condition: expect.any(Function),
         component: expect.anything()
