f3ba09663f29ddacd17c576de2168af654f96fc7
Paul Tran-Van <paul.tranvan@gmail.com>
2026-03-20 14:40:18 +0100

    feat: Use Twake icon for default AI
    We used to display the OpenRAG logo, let's use Twake instead.

--- packages/cozy-search/src/components/CreateAssistantSteps/Provider.jsx
+++ packages/cozy-search/src/components/CreateAssistantSteps/Provider.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import CheckIcon from 'cozy-ui/transpiled/react/Icons/Check'
 import PlusSmallIcon from 'cozy-ui/transpiled/react/Icons/PlusSmall'
+import TwakeLogo from 'cozy-ui/transpiled/react/Icons/TwakeWorkplace'
 import Typography from 'cozy-ui/transpiled/react/Typography'
-import { useI18n } from 'twake-i18n'
 import { useCozyTheme } from 'cozy-ui-plus/dist/providers/CozyTheme'
+import { useI18n } from 'twake-i18n'
 
 import styles from './styles.styl'
 import AnthropicLogo from '../../assets/anthropic.svg'
@@ -14,10 +15,9 @@ import GeminiLogo from '../../assets/gemini.svg'
 import MetaLogo from '../../assets/meta.svg'
 import MistralLogo from '../../assets/mistral.svg'
 import OpenAILogo from '../../assets/open_ai.svg'
-import OpenRagLogo from '../../assets/openrag.png'
 
 const ICONS = {
-  OpenRagLogo,
+  TwakeLogo,
   GeminiLogo,
   OpenAILogo,
   MistralLogo,
@@ -52,7 +52,14 @@ const Provider = ({ selectedProvider, provider, onSelect }) => {
           styles[`icon-container--${provider.id}`]
         )}
       >
-        <Icon icon={ICONS[provider.icon]} size={40} />
+        {
+          /* FIXME: the twake icon is not rendered properly with Icon */
+          isOpenRag ? (
+            <TwakeLogo width={40} height={40} />
+          ) : (
+            <Icon icon={ICONS[provider.icon]} size={40} />
+          )
+        }
       </div>
       <div className="u-flex-grow-1">
         <Typography
