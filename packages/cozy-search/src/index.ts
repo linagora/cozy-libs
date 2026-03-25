3487ff9f4cd2630f61b4c88587cf6872243e6b36
Paul Tran-Van <paul.tranvan@gmail.com>
2026-03-25 23:14:23 +0100

    feat: export tools API surface

--- packages/cozy-search/src/index.ts
+++ packages/cozy-search/src/index.ts
@@ -1,16 +1,35 @@
-export { default as AssistantLink } from './components/Search/AssistantLink'
-export { default as AssistantDesktop } from './components/AssistantDesktop'
-export { default as AssistantMobile } from './components/AssistantMobile'
-export { default as AssistantDialog } from './components/Views/AssistantDialog'
-export { default as CreateAssistantDialog } from './components/Views/CreateAssistantDialog'
-export { default as SearchDialog } from './components/Views/SearchDialog'
-export { default as AiText } from './components/Icons/AiText'
+export { default as AssistantLink } from "./components/Search/AssistantLink";
+export { default as AssistantDesktop } from "./components/AssistantDesktop";
+export { default as AssistantMobile } from "./components/AssistantMobile";
+export { default as AssistantDialog } from "./components/Views/AssistantDialog";
+export { default as CreateAssistantDialog } from "./components/Views/CreateAssistantDialog";
+export { default as SearchDialog } from "./components/Views/SearchDialog";
+export { default as AiText } from "./components/Icons/AiText";
 
 // New assistant-ui based components
-export { default as CozyAssistantRuntimeProvider } from './components/CozyAssistantRuntimeProvider'
-export { default as CozyComposer } from './components/Conversations/ConversationComposer'
+export { default as CozyAssistantRuntimeProvider } from "./components/CozyAssistantRuntimeProvider";
+export { default as CozyComposer } from "./components/Conversations/ConversationComposer";
 export {
   StreamBridge,
-  createCozyRealtimeChatAdapter
-} from './components/adapters'
-export { default as AssistantView } from './components/Views/AssistantView'
+  createCozyRealtimeChatAdapter,
+} from "./components/adapters";
+export { default as AssistantView } from "./components/Views/AssistantView";
+
+// Tools API
+export type {
+  AssistantTool,
+  ConfirmationDetails,
+  ToolResult,
+  ToolSchema,
+  ToolCall,
+} from "./tools/types";
+export { getPlatformTools, findToolByName } from "./tools/registry";
+export {
+  toToolSchemas,
+  validateToolArgs,
+  parseSlashCommand,
+} from "./tools/helpers";
+export {
+  default as AppToolsProvider,
+  useAppTools,
+} from "./components/AppToolsProvider";
