e32d47586bf5c76b2e42267e6137d7151d2b6ff9
Théo Poizat <hello@zatteo.com>
2026-03-25 08:06:15 +0100

    feat(cozy-bar): Update jest configuration

--- packages/cozy-bar/jest.config.js
+++ packages/cozy-bar/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost/'
+  },
   moduleFileExtensions: ['js', 'jsx', 'json', 'styl', 'yaml'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   setupFiles: ['<rootDir>/test/jestLib/setup.js'],
@@ -12,14 +16,11 @@ module.exports = {
     '\\.(png|gif|jpe?g|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.styl$': 'identity-obj-proxy',
     '\\.css$': 'identity-obj-proxy',
-    '^test/(.*)': '<rootDir>/test/$1'
+    '^test/(.*)': '<rootDir>/test/$1',
+    '^cozy-client$': '<rootDir>/node_modules/cozy-client/dist/index',
+    '^cozy-client/dist/(.*)$': '<rootDir>/node_modules/cozy-client/dist/$1',
+    '^react$': '<rootDir>/node_modules/react',
+    '^react-dom(.*)$': '<rootDir>/node_modules/react-dom$1'
   },
-  browser: true,
-  transformIgnorePatterns: ['node_modules/(?!cozy-ui)'],
-  globals: {
-    __ALLOW_HTTP__: false,
-    __SENTRY_TOKEN__: 'token',
-    __DEVELOPMENT__: false,
-    __VERSION__: '4.5.0'
-  }
+  transformIgnorePatterns: ['node_modules/(?!cozy-ui)']
 }
