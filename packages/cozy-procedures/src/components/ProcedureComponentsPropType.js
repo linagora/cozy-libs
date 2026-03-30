30515fa2e7936e8a8b73ad4760ddb277137842c4
Théo Poizat <hello@zatteo.com>
2026-03-30 08:02:29 +0200

    chore: Remove cozy-procedures package
    Unused

--- packages/cozy-procedures/src/components/ProcedureComponentsPropType.js
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types'
-
-export default PropTypes.shape({
-  PageLayout: PropTypes.elementType.isRequired,
-  PageFooter: PropTypes.elementType.isRequired,
-  PageContent: PropTypes.elementType.isRequired
-})
--- packages/cozy-procedures/test/__mocks__/tMock.js
+++
@@ -1,5 +0,0 @@
-const tMock = (msgId, args) => {
-  return `${msgId} ${args ? JSON.stringify(args) : ''}`
-}
-
-export default tMock
