919a7bf5a87cc8d2c216d9aaf7fda78213d75d3b
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-04-16 15:46:09 +0200

    feat: Add EmailDomainChip component
    to show a warning chip if email domain is not configured

---
+++ packages/cozy-bar/src/components/UserMenu/EmailDomainChip.jsx
@@ -0,0 +1,44 @@
+import { buildAppsQuery } from 'queries'
+import React from 'react'
+
+import { useInstanceInfo } from 'cozy-client'
+import { useQuery, hasQueryBeenLoaded } from 'cozy-client'
+import Chip from 'cozy-ui/transpiled/react/Chips'
+import { useI18n } from 'twake-i18n'
+
+const EmailDomainChip = () => {
+  const { t } = useI18n()
+  const {
+    instance: { data: instanceData },
+    isLoaded: isInstanceInfoLoaded
+  } = useInstanceInfo()
+
+  const appsQuery = buildAppsQuery()
+  const appsResult = useQuery(appsQuery.definition, appsQuery.options)
+  const isAppsQueryLoaded = hasQueryBeenLoaded(appsResult)
+
+  if (!isInstanceInfoLoaded || !isAppsQueryLoaded) {
+    return null
+  }
+
+  const isMailAppInstalled = appsResult.data.some(app =>
+    app.slug.includes('mail')
+  )
+  const isShown = !!instanceData.org_id && !isMailAppInstalled
+
+  if (!isShown) {
+    return null
+  }
+
+  return (
+    <div className="u-flex u-flex-justify-center u-mt-half">
+      <Chip
+        variant="ghost"
+        color="warning"
+        label={t('userMenu.emailDomainNotActive')}
+      />
+    </div>
+  )
+}
+
+export default EmailDomainChip
