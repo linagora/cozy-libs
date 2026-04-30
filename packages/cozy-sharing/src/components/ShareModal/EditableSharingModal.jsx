e10d3d83b304c8b3f55b8ba6ea00a29af9415f8b
Théo Poizat <hello@zatteo.com>
2026-04-30 08:33:48 +0200

    refactor(cozy-sharing): Remove unused SharedDriveEditModal
    We have a root ShareModal that redirect to the appropriate modal depending of the sharing and flags.
    
    ShareModal:
    - correspond to "old" sharing
    - work on existing folder
    - use sharing by link or cozy to cozy sharing
    => in this commit, we remove a dead path where this Share modal supported part of shared drive specification
    
    FederatedFolderModal:
    - correspond to "new" sharing
    - work on existing folder
    - use sharing by link or federated sharing

--- packages/cozy-sharing/src/components/ShareModal/EditableSharingModal.jsx
+++ packages/cozy-sharing/src/components/ShareModal/EditableSharingModal.jsx
@@ -21,7 +21,6 @@ export const EditableSharingModal = ({ document, ...rest }) => {
     hasSharedChild,
     hasSharedParent,
     isOwner,
-    isSharedDrive,
     revoke,
     revokeSelf,
     share
@@ -35,7 +34,6 @@ export const EditableSharingModal = ({ document, ...rest }) => {
       hasSharedChild={documentPath && hasSharedChild(documentPath)}
       hasSharedParent={documentPath && hasSharedParent(documentPath)}
       isOwner={isOwner(document._id)}
-      isSharedDrive={isSharedDrive(document._id)}
       link={getSharingLink(document._id)}
       onRevoke={revoke}
       onRevokeSelf={revokeSelf}
