b0677189eeebe258177187ea98ce2f48b6af8777
Théo Poizat <hello@zatteo.com>
2026-06-17 10:58:40 +0200

    feat: Remove unwanted details from sharing details modal

--- packages/cozy-sharing/src/components/ShareModal/SharingDetailsModal.jsx
+++ packages/cozy-sharing/src/components/ShareModal/SharingDetailsModal.jsx
@@ -1,29 +1,21 @@
-import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
-import Divider from 'cozy-ui/transpiled/react/Divider'
-import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'twake-i18n'
 
-import { getDisplayName } from '../../models'
 import styles from '../../styles/share.styl'
-import OwnerIdentity from '../Identity/OwnerIdentity'
 import WhoHasAccess from '../WhoHasAccess'
 
 export const SharingDetailsModal = ({
   onClose,
-  sharingType,
-  owner,
   recipients,
   document,
   documentType = 'Document',
   onRevoke,
   onRevokeSelf
 }) => {
-  const { t, f } = useI18n()
-  const { isMobile } = useBreakpoints()
+  const { t } = useI18n()
 
   return (
     <Dialog
@@ -34,45 +26,7 @@ export const SharingDetailsModal = ({
       title={t(`${documentType}.share.details.title`)}
       content={
         <div className={styles['share-modal-content']}>
-          <div
-            className={cx(
-              styles['share-details'],
-              isMobile ? 'u-ph-1 u-pt-1-half' : 'u-ph-2 u-pt-1'
-            )}
-          >
-            {owner ? (
-              <OwnerIdentity
-                name={t(`${documentType}.share.sharedBy`, {
-                  name: getDisplayName(owner)
-                })}
-                url={owner.instance}
-              />
-            ) : null}
-            <div className={styles['share-details-created']}>
-              {t(`${documentType}.share.details.createdAt`, {
-                date: f(document.created_at || null, 'do LLLL yyyy')
-              })}
-            </div>
-            <div className={styles['share-details-perm']}>
-              {t(
-                `${documentType}.share.details.${
-                  sharingType === 'one-way' ? 'ro' : 'rw'
-                }`
-              )}
-            </div>
-            <div className={styles['share-details-perm-desc']}>
-              {t(
-                `${documentType}.share.details.desc.${
-                  sharingType === 'one-way' ? 'ro' : 'rw'
-                }`
-              )}
-            </div>
-          </div>
-          <Divider className="u-mv-1" />
           <WhoHasAccess
-            title={t('Share.recipients.accessCount', {
-              count: recipients.length
-            })}
             recipients={recipients}
             document={document}
             documentType={documentType}
