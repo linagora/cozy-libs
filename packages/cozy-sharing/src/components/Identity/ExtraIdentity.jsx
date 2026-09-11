3aebdf8c1a1ca592ebc685eb1f6cc9a9be7a121b
Théo Poizat <hello@zatteo.com>
2026-09-11 19:33:29 +0200

    refactor: Use t from useI18n
    To clean I18nProvider.

--- packages/cozy-sharing/src/components/Identity/ExtraIdentity.jsx
+++ packages/cozy-sharing/src/components/Identity/ExtraIdentity.jsx
@@ -1,28 +1,29 @@
-import PropTypes from 'prop-types'
 import React from 'react'
+
+import { useI18n } from 'twake-i18n'
 
 import Identity from './Identity'
 import { getDisplayName } from '../../models'
 import styles from '../../styles/recipient.styl'
 import { ExtraAvatar } from '../Avatar/ExtraAvatar'
 
-const ExtraIdentity = ({ extraRecipients }, { t }) => (
-  <div className={styles['recipient']}>
-    <ExtraAvatar
-      extraRecipients={extraRecipients.map(recipient =>
-        getDisplayName(recipient)
-      )}
-    />
-    <div className={styles['recipient-ident-status']}>
-      <Identity
-        name={t('Share.members.otherContacts', extraRecipients.length)}
-      />
-    </div>
-  </div>
-)
+const ExtraIdentity = ({ extraRecipients }) => {
+  const { t } = useI18n()
 
-ExtraIdentity.contextTypes = {
-  t: PropTypes.func.isRequired
+  return (
+    <div className={styles['recipient']}>
+      <ExtraAvatar
+        extraRecipients={extraRecipients.map(recipient =>
+          getDisplayName(recipient)
+        )}
+      />
+      <div className={styles['recipient-ident-status']}>
+        <Identity
+          name={t('Share.members.otherContacts', extraRecipients.length)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export { ExtraIdentity }
