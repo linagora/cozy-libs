b38be061459cee1cae9de272e728710bcafe3db1
Théo Poizat <hello@zatteo.com>
2026-09-11 19:41:10 +0200

    feat: Remove unused internal export

--- packages/cozy-harvest-lib/src/components/hoc/withLocales.js
+++ packages/cozy-harvest-lib/src/components/hoc/withLocales.js
@@ -1,6 +1,4 @@
-import I18n, { withLocales } from 'twake-i18n'
+import { withLocales } from 'twake-i18n'
 const dictRequire = lang => require(`../../locales/${lang}.json`)
-
-export const i18nContextTypes = I18n.childContextTypes
 
 export default withLocales(dictRequire)
